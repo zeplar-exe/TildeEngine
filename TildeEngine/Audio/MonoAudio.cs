373378a682e24cc21fc01827ea822cf805e2e7f8
Zander Baker <zanderbaker100@gmail.com>
2021-12-20 22:16:44 -0800

    Create Audio/MonoAudio; No functionality

---
+++ TildeEngine/Audio/MonoAudio.cs
@@ -0,0 +1,14 @@
+namespace TildeEngine.Audio;
+
+public static class MonoAudio
+{
+    public static void Play(byte[] audio)
+    {
+        // Something something CSCore IWaveSource
+    }
+
+    public static void Play(Stream stream)
+    {
+        
+    }
+}
