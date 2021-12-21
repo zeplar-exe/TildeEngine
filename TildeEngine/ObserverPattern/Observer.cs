908b4c9c27a7e2b842e5ef2546aef98f8a9abfa1
Zander Baker <zanderbaker100@gmail.com>
2021-12-20 22:31:35 -0800

    Add invocation buffer to Observer which is useful for logging

--- TildeEngine/ObserverPattern/Observer.cs
+++ TildeEngine/ObserverPattern/Observer.cs
@@ -2,12 +2,15 @@ namespace TildeEngine.ObserverPattern;
 
 public static class Observer
 {
+    public static Queue<(Enum, object?)> LogBuffer { get; } = new();
+
     public delegate void ObserverEventHandler(Enum e, object? o);
     public static event ObserverEventHandler? Event;
 
     public static void Invoke<TEnum, TParameter>(TEnum e, TParameter param) where TEnum : Enum
     {
         Event?.Invoke(e, param);
+        LogBuffer.Enqueue((e, param));
     }
 
     public static ObserverHook Hook<TEnum, TParameter>(TEnum e, Action<TParameter> action) where TEnum : Enum
